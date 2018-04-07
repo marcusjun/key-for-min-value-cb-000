26192f8b11e8f75bb6b9e9f11339bdfa461af4eb
Marcus Jun <mjun@calstate.edu>
2018-04-07 21:50:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,20 @@
 
 def key_for_min_value(name_hash)
 
+  value_array= name_hash.collect {|key,value| value}
+  key_array= name_hash.collect {|key,value| key}
+
+  min_value=value_array[0]
+  min_key=key_array[0]
+
+  if name_hash=={}
+    nil
+  else
+    name_hash.each do |key,value|
+      if min_value>name_hash[key]
+        min_key=key
+      end
+    end
+    min_key
+  end
 end
